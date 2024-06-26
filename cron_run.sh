1165dab7a8d025266785c09588afc35dafa74e8b
Yan Zhou <Yan.Zhou2@nike.com>
2024-06-26 10:52:51 +0800

    GCLAUNCH-588:change job order

--- cron_run.sh
+++ cron_run.sh
@@ -4,11 +4,11 @@ cd /opt/pyth/crawler
 source .venv/bin/activate
 PYTHON_CMD=`which python`
 
-echo "Updating reviews..."
-${PYTHON_CMD} update_reviews.py || exit 1
-echo "Updating prices..."
-${PYTHON_CMD} update_prices.py || exit 2
-echo "Updating ratings..."
-${PYTHON_CMD} rating_calc.py || exit 3
 echo "Syncing products..."
-${PYTHON_CMD} sync_launch_product.py || exit 4
+${PYTHON_CMD} sync_launch_product.py || exit 1
+echo "Updating reviews..."
+${PYTHON_CMD} update_reviews.py || exit 2
+echo "Updating prices..."
+${PYTHON_CMD} update_prices.py || exit 3
+echo "Updating ratings..."
+${PYTHON_CMD} rating_calc.py || exit 4
